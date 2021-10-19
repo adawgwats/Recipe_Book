4603808a22dc45eda518255c82a39c9ff1c629e2
root <root@MSI.localdomain>
2021-10-19 15:20:49 -0400

    User prompted to input which recipe they want after passing the all command to driver_retriever.java

--- Database_API/driver_retriever.java
+++ Database_API/driver_retriever.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.io.*;
 class driver_retriever{
     public static void main(String[] args){
         Data_Retriever dr = new Data_Retriever("user", "abc1234", "sample_data.csv");
@@ -19,6 +21,14 @@ class driver_retriever{
                     System.out.println(e);
                 }
             }
+            else if (val.startsWith("stepingr")){
+                try{
+                    dr.get_ingredients(val.split("_")[1]);
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+            }
             else if (val.startsWith("inst")){
                 try{
                     System.out.println(dr.get_instructions(val.split("_")[1]));
@@ -26,6 +36,22 @@ class driver_retriever{
                 catch (Exception e){
                     System.out.println(e);
                 }
+            }
+            else if(val.startsWith("all")){
+                Enumeration<String> food_item = dr.file_data.keys();
+                while (food_item.hasMoreElements()){ 
+                    String key = food_item.nextElement();
+                    if (!dr.file_data.get(key)[0].equals("\n")){
+                        System.out.println(dr.file_data.get(key)[0]);
+
+                    }
+
+                }
+                System.out.println("Which recipe would you like to know about? ");
+                Scanner sc = new Scanner(System.in);
+                String recipe = sc.nextLine();
+                System.out.println(dr.get_ingredients(recipe));
+
             }
             else{
                 System.out.println(val + " is not a valid command");
